6a13c1edd362a4bb0f7aaa8ce7d0e8e31d3dcc08
pirateking707 <97656392+pirateking707@users.noreply.github.com>
2022-01-14 21:09:13 -0800

    Update signup_process.php

--- signup_process.php
+++ signup_process.php
@@ -1,12 +1,53 @@
 <?php
+   error_reporting(0);
 
-    // CREATE TABLE ecommerce (
-    //     Username VARCHAR(25),
-    //     Password VARCHAR(25)
-    // );
+   /* Check for empty first...*/
+   if(empty($_POST['name'])){
+      echo 'You left name field blank!';
+      exit();
+   }
 
-    session_start();
-    $_SESSION["username"] = $username;
-    header("Location: login_page.html");
+   if(empty($_POST['reciever'])){
+      echo 'You left name field blank!';
+      exit();
+   }
+
+   /* Retrieve form data into PHP variables */
+   $name = $_POST['name'];
+   $email = $_POST['reciever'];
+   $message = $_POST['message'];
+   $name = test_input($name);
+   $email = test_input($email);
+   $message = test_input($message);
+
+   /* Validate e-mail...*/
+   if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      echo 'Invalid email format.';
+      exit();
+   }
+
+   /* Validate message...*/
+   if(strlen($message) < 10){
+      echo 'Message must be at least 10 characters long.';
+      exit();
+   }
+
+   /* Set uo and send email to our web server...*/
+   $subject = 'Contact Us';
+   $recipient = 'ouremail@domain.com'; /* Replace with our own email */
+   $msg = sprintf('Retrun address: %s \n\nMessage: %s', $email, $message);
+   if(mail($recipient, $subject, $msg)){
+      echo 'Mail successfully sent!';
+      exit();
+   }else{
+      echo 'Error sending email.';
+      exit();
+   }
+
+   function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+   }
 ?>
-
